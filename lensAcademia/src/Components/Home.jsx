69e6619048a74e2fbd7408ec5b4aa30a7843403f
Shay <fwasil83@gmail.com>
2023-08-01 13:13:52 -0400

    redo

--- lensAcademia/src/Components/Home.jsx
+++ lensAcademia/src/Components/Home.jsx
@@ -1,250 +1,75 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Carousel, Form, Table } from 'react-bootstrap';
-import Transitions from './Transition';
+import React, { useState, useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import '../App.css'
 
 const Home = () => {
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: '100%',
-    color: '#CFCBC9',
-    margin: '0 auto',
-    fontFamily: 'Prata',
-  };
+  const [researchPapers, setResearchPapers] = useState([]);
 
-  const contentStyle = {
-    maxWidth: '800px',
-    margin: '0 auto',
-    padding: '50px',
-    backgroundColor: '#A6A0A0',
-    color: '#1E1E3A',
-    boxShadow: '0 10px 10px black',
-  };
+  useEffect(() => {
+    // Fetch the research papers from the API (adjust the URL as needed)
+    fetch('http://127.0.0.1:8000/api/researchpapers/')
+      .then((response) => response.json())
+      .then((data) => setResearchPapers(data))
+      .catch((error) => console.error('Error fetching research papers:', error));
+  }, []);
 
-  const headingStyle = {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    marginBottom: '16px',
-    marginTop: '1vmin',
-  };
-
-  const leadStyle = {
-    fontSize: '18px',
-    lineHeight: '1.6',
-    marginBottom: '20px',
-    paddingLeft: '4rem',
-    paddingRight: '4rem',
-  };
-
-  const paragraphStyle = {
-    fontSize: '18px',
-    lineHeight: '1.5',
-    marginBottom: '10px',
-    paddingLeft: '4rem',
-    paddingRight: '4rem',
-  };
-
-  const tableStyle = {
+  const carouselStyle = {
     width: '100%',
-    marginTop: '20px',
-    textDecoration: 'none',
-  };
-
-  const thStyle = {
-    backgroundColor: '#A6A0A0',
-    textDecoration: 'none',
-  };
-
-  const tableRowStyle = {
-    fontSize: '14x',
-    textDecoration: 'none',
+    margin: 0,
+    padding: 0,
+    position: 'relative',
+    backgroundColor: '#DFDEDE',
+    marginBottom: '10vmin',
+    marginTop: '-2.5vmin', 
   };
 
   const carouselItemStyle = {
+    width: '100%',
+  };
+
+  const carouselCaptionStyle = {
+    color: '#fff',
+    textAlign: 'left',
+    backgroundColor: '#A6A0A0',
+    padding: '80px',
+  };
+
+  const abstractStyle = {
+    display: '-webkit-box',
+    WebkitLineClamp: '5',
+    WebkitBoxOrient: 'vertical',
     overflow: 'hidden',
-    backgroundColor: 'rgba(65, 60, 58, 0.8)',
-    backdropFilter: 'blur(8px)',
+    textOverflow: 'ellipsis',
+    marginBottom: '10px',
   };
-
-  const [researchPapers, setResearchPapers] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/researchpapers/')
-      .then((response) => response.json())
-      .then((data) => {
-        setResearchPapers(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching research papers:', error);
-        setLoading(false);
-      });
-  }, []);
-
-  const handleReadMoreClick = (url) => {
-    window.open(url, '_blank');
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const filteredPapers = researchPapers.filter((paper) => {
-    const keywords = paper.keywords.map((keyword) => keyword.word.toLowerCase());
-    const authors = paper.researchers.map((researcher) => researcher.name.toLowerCase());
-
-    return (
-      paper.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      paper.abstract.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      paper.country.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      keywords.includes(searchQuery.toLowerCase()) ||
-      authors.some((author) => author.includes(searchQuery.toLowerCase()))
-    );
-  });
-
-  const carouselRef = useRef();
-
-  const handleKeyboardNavigation = (event) => {
-    if (event.keyCode === 37) {
-      carouselRef.current.prev();
-    } else if (event.keyCode === 39) {
-      carouselRef.current.next();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyboardNavigation);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyboardNavigation);
-    };
-  }, []);
-
-  const hasFilteredPapers = filteredPapers.length > 0;
-  const showTable = searchQuery !== '' && hasFilteredPapers;
 
   return (
-    <Transitions>
-      <div style={containerStyle}>
-        {loading ? (
-          <div> </div>
-        ) : (
-          <div className="carousel-container" style={{ height: '400px' }}>
-            <Carousel
-              ref={carouselRef}
-              interval={null}
-              indicators={false}
-              prevIcon={<span className="carousel-arrow">‹</span>}
-              nextIcon={<span className="carousel-arrow">›</span>}
-              className="carousel"
-            >
-              {researchPapers.map((paper) => (
-                <Carousel.Item key={paper.id} style={carouselItemStyle}>
-                  <div style={contentStyle}>
-                    <h3>{paper.name}</h3>
-                    <p className="carousel-abstract">{paper.abstract}</p>
-                    {paper.abstract.length > 100 && (
-                      <span
-                        style={{ textDecoration: 'underline', cursor: 'pointer' }}
-                        onClick={() => handleReadMoreClick(paper.url)}
-                      >
-                        Read More
-                      </span>
-                    )}
-                  </div>
-                </Carousel.Item>
-              ))}
-            </Carousel>
-          </div>
-        )}
-        <div>
-          <h2 style={headingStyle}>Welcome to LensAcademia!</h2>
-          <p style={leadStyle}>
-            LensAcademia is a platform that provides access to academic research papers and authors. It is designed to be a comprehensive resource for researchers, students, and anyone interested in exploring scholarly articles from various disciplines. Our vast database includes a wide range of research papers on subjects such as physics, chemistry, biology, mathematics, literature, history, and more.
-          </p>
-          <p style={paragraphStyle}>
-            To get started, use the search bar below to find research papers related to your area of interest. You can search by paper title, author name, country of origin, or keywords associated with the paper. Our advanced search capabilities make it easy to discover relevant academic content quickly. Start your journey with LensAcademia today and unlock a world of academic insights at your fingertips!
-          </p>
-        </div>
-
-        <Form style={{ marginBottom: '20px' }}>
-          <Form.Control
-            type="text"
-            placeholder="Search"
-            value={searchQuery}
-            onChange={handleSearchChange}
-          />
-        </Form>
-
-        {showTable && (
-          <Table striped bordered hover responsive style={tableStyle}>
-            <thead>
-              <tr>
-                <th style={{ ...thStyle, backgroundColor: '#A6A0A0' }}>Title</th>
-                <th style={{ ...thStyle, backgroundColor: '#A6A0A0' }}>Abstract</th>
-                <th style={{ ...thStyle, backgroundColor: '#A6A0A0' }}>Country</th>
-                <th style={{ ...thStyle, backgroundColor: '#A6A0A0' }}>Authors</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredPapers.map((paper) => (
-                <tr key={paper.id} style={tableRowStyle}>
-                  <td style={{ backgroundColor: '#CFCBC9' }}>
-                    <a
-                      href={paper.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ textDecoration: 'none', color: '#141B41' }}
-                    >
-                      {paper.name}
-                    </a>
-                  </td>
-                  <td style={{ backgroundColor: '#CFCBC9' }}>
-                    <a
-                      href={paper.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ textDecoration: 'none', color: '#141B41' }}
-                    >
-                      {paper.abstract}
-                    </a>
-                  </td>
-                  <td style={{ backgroundColor: '#CFCBC9' }}>
-                    <a
-                      href={paper.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ textDecoration: 'none', color: '#141B41' }}
-                    >
-                      {paper.country}
-                    </a>
-                  </td>
-                  <td style={{ backgroundColor: '#CFCBC9' }}>
-                    {paper.researchers.map((researcher) => (
-                      <span key={researcher.id}>
-                        <a
-                          href={researcher.url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          style={{ textDecoration: 'none', color: '#141B41' }}
-                        >
-                          {researcher.name}
-                        </a>
-                        <br />
-                      </span>
-                    ))}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        )}
-      </div>
-    </Transitions>
+    <div className="container mt-4">
+      {researchPapers.length === 0 ? (
+        <p>No research papers available.</p>
+      ) : (
+        <Carousel style={carouselStyle}>
+          {researchPapers.map((paper) => (
+            <Carousel.Item key={paper.id} style={carouselItemStyle}>
+              {paper.image ? (
+                <img className="d-block w-100" src={paper.image} alt={paper.name} />
+              ) : (
+                <div className="d-block w-100 placeholder" style={{ height: '400px' }}></div>
+              )}
+              <Carousel.Caption style={carouselCaptionStyle}>
+                <h3>{paper.name}</h3>
+                <p style={abstractStyle}>{paper.abstract}</p>
+                <a href={paper.url} target="_blank" rel="noreferrer" className="btn btn-primary">
+                  Read More
+                </a>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )}
+      <h1>Welcome to Lens Academia</h1>
+      <p className="mb-4">Explore our research papers and stay updated with the latest findings.</p>
+    </div>
   );
 };
 
--- lensAcademia/src/Components/PostResearchPaper.jsx
+++ lensAcademia/src/Components/PostResearchPaper.jsx
@@ -1,110 +1,58 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Form, Button, Container } from 'react-bootstrap';
-import Transtions2 from './Transitions2'
 
-function getCookie(name) {
-  const cookieValue = document.cookie
-    .split('; ')
-    .find(cookie => cookie.startsWith(name + '='));
-  
-  if (cookieValue) {
-    return cookieValue.split('=')[1];
-  }
-  
-  return null;
-}
-
-const PostResearchPaper = () => {
-  const [formData, setFormData] = useState({
+const PostResearchPaperForm = () => {
+  const initialFormState = {
+    tg: 'http://127.0.0.1:8000/api/tgs/6/',
+    researchers: [],
+    keywords: [],
     name: '',
-    researchers:'',
     abstract: '',
     url: '',
     country: '',
-    tg: '',
-    // keywords: '',
-   
-  });
+  };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+  const [formData, setFormData] = useState(initialFormState);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const csrftoken = getCookie('csrftoken');
-
-    axios
-      .post(
-        'http://127.0.0.1:8000/api/researchpapers/',
-        formData,
-        {
-          headers: {
-            'X-CSRFToken': csrftoken,
-          },
-        }
-      )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://127.0.0.1:8000/api/researchpapers/', formData)
       .then((response) => {
-        alert('Research paper successfully posted!');
-         
+        console.log('Research paper posted:', response.data);
+        // Reset the form after successful submission
+        setFormData(initialFormState);
       })
       .catch((error) => {
-        alert('An error occurred while posting the research paper.');
-        console.error('Error:', error);
+        console.error('Error posting research paper:', error);
       });
   };
 
   return (
-    <Container>
-        <Transtions2>
-      <h1 style={{ color:'#A6A0A0', textAlign: 'center', marginTop:'2rem' }}>Submit a New Paper</h1>
-      <Form onSubmit={handleSubmit} style={{ color:'#A6A0A0', maxWidth: '600px', margin: '0 auto' }}>
-        <Form.Group>
-          <Form.Label>Name:</Form.Label>
-          <Form.Control type="text" name="name" value={formData.name} onChange={handleInputChange} required style={{ backgroundColor:'#A6A0A0'}}></Form.Control> 
-        </Form.Group>
-
-          <Form.Group>
-          <Form.Label>Researchers:</Form.Label>
-          <Form.Control type="text" name="researchers" value={formData.researchers} onChange={handleInputChange} required style={{ backgroundColor:'#A6A0A0'}} />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Abstract:</Form.Label>
-          <Form.Control as="textarea" rows="4" name="abstract" value={formData.abstract} onChange={handleInputChange} required style={{ backgroundColor:'#A6A0A0'}}/>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>URL:</Form.Label>
-          <Form.Control type="url" name="url" value={formData.url} onChange={handleInputChange} required style={{ backgroundColor:'#A6A0A0'}}/>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Country:</Form.Label>
-          <Form.Control type="text" name="country" value={formData.country} onChange={handleInputChange} required style={{ backgroundColor:'#A6A0A0'}}/>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Topic Group:</Form.Label>
-          <Form.Control type="text" name="tg" value={formData.tg} onChange={handleInputChange} required style={{ backgroundColor:'#A6A0A0'}}/>
-        </Form.Group>
-
-        {/* <Form.Group>
-          <Form.Label>Keywords:</Form.Label>
-          <Form.Control type="text" name="keywords" value={formData.keywords} onChange={handleInputChange} required />
-        </Form.Group> */}
-
-      
-
-        <div style={{ textAlign: 'center' }}>
-          <Button type="submit" style={{ padding: '10px 20px', fontSize: '16px', backgroundColor: '#A6A0A0', color: 'white', border: 'none', marginTop:'1rem', borderRadius: '4px', cursor: 'pointer' }}>Submit</Button>
-        </div>
-      </Form>
-      </Transtions2>
-    </Container>
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">Name</label>
+        <input type="text" className="form-control" id="name" name="name" value={formData.name} onChange={handleChange} />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="abstract" className="form-label">Abstract</label>
+        <textarea className="form-control" id="abstract" name="abstract" value={formData.abstract} onChange={handleChange} />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="url" className="form-label">URL</label>
+        <input type="text" className="form-control" id="url" name="url" value={formData.url} onChange={handleChange} />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="country" className="form-label">Country</label>
+        <input type="text" className="form-control" id="country" name="country" value={formData.country} onChange={handleChange} />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
   );
 };
 
-export default PostResearchPaper;
+export default PostResearchPaperForm;
